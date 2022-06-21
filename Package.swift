96860e414754f0ef7d091743607afd9e6e6e345d
Daven Quinn <dev@davenquinn.com>
2022-06-21 15:21:42 -0500

    Updated public-facing attributes

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftTileMatrix",
-            dependencies: []),
+            dependencies: ["GEOSwift"]),
         .testTarget(
             name: "SwiftTileMatrixTests",
             dependencies: ["SwiftTileMatrix"]),
--- Sources/SwiftTileMatrix/SwiftTileMatrix.swift
+++ Sources/SwiftTileMatrix/SwiftTileMatrix.swift
@@ -4,14 +4,14 @@ import GEOSwift
 
 private let latMax = 2*atan(exp(Double.pi))-Double.pi/2
 
-let earthRadius: Double = 6378137
+public let earthRadius: Double = 6378137
 let gridSize = 2*Double.pi*earthRadius
 
 enum GeographyError: Error {
   case invalidTileIndex(_ x: Int, _ y: Int, _ z: Int)
 }
 
-func webMercatorScale(zoom: Double, latitude: Double, tileSize: Double = 512)->Double {
+public func webMercatorScale(zoom: Double, latitude: Double, tileSize: Double = 512)->Double {
   //meters per pixel
   let prefix = gridSize / tileSize
   let zoomTerm = pow(2,zoom)
@@ -19,11 +19,11 @@ func webMercatorScale(zoom: Double, latitude: Double, tileSize: Double = 512)->D
   return prefix * trigTerm / zoomTerm
 }
 
-func webMercatorScale(_ point: Point, zoom: Int, scale: Int = 2)->Float {
+public func webMercatorScale(_ point: Point, zoom: Int, scale: Int = 2)->Float {
   return Float(webMercatorScale(zoom: Double(zoom), latitude: Double(point.y), tileSize: Double(256*scale)))
 }
 
-extension CLLocationCoordinate2D {
+public extension CLLocationCoordinate2D {
   init(webMercatorPoint pt: Point) {
     let scalar = Double.pi / 180 * earthRadius
     self.init(latitude: pt.y/scalar, longitude: pt.x/scalar)
@@ -39,6 +39,11 @@ func tileEnvelope(x: Double, y: Double, z: Double) throws -> Envelope {
   if x < 0 || x >= nTiles || y < 0 || y >= nTiles {
     throw GeographyError.invalidTileIndex(Int(x),Int(y),Int(z))
   }
+  return _tileEnvelope(x: x, y: y, z: z)
+}
+
+private func _tileEnvelope(x: Double, y: Double, z: Double) -> Envelope {
+  let nTiles = pow(2, z)
   
   let cx = gridSize/2
   let cy = gridSize/2
@@ -55,12 +60,18 @@ func tileEnvelope(x: Double, y: Double, z: Double) throws -> Envelope {
   return env
 }
 
-struct TileCoord {
-  let x: Int
-  let y: Int
-  let z: Int
+public struct TileCoord {
+  public let x: Int
+  public let y: Int
+  public let z: Int
   
-  var center: CLLocationCoordinate2D {
+  public init(_ x: Int, _ y: Int, _ z: Int) {
+    self.x = x
+    self.y = y
+    self.z = z
+  }
+  
+  public var center: CLLocationCoordinate2D {
     let cc = gridSize/2
     let tileSizeMeters = gridSize/pow(2,Double(z))
     let cx = cc-tileSizeMeters*(Double(x)+0.5)
@@ -69,8 +80,12 @@ struct TileCoord {
     return CLLocationCoordinate2D(latitude: cy/scalar, longitude: cx/scalar)
   }
   
-  func estimatedPixelScale(tileSize: Double = 512)->Double {
+  public func estimatedPixelScale(tileSize: Double = 512)->Double {
     // meters per pixel at the center of the tile
     return webMercatorScale(zoom: Double(self.z), latitude: self.center.latitude, tileSize: tileSize)
+  }
+  
+  public var envelope: Envelope {
+    return _tileEnvelope(x: Double(x), y: Double(y), z: Double(z))
   }
 }
